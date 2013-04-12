0462a4d506c0662a37b91aa84a364849730a0ea9
Luke Campbell <luke.s.campbell@gmail.com>
2013-04-12 16:15:40 -0400

    Fix

--- ion_functions/__init__.py
+++ ion_functions/__init__.py
@@ -4,5 +4,4 @@ from utils import isempty, islogical, ismatrix, isnumeric, isreal, isscalar, isv
 from qc.qc_functions import dataqc_condcompress, dataqc_globalrangetest, dataqc_globalrangetest_minmax, dataqc_gradienttest, dataqc_localrangetest, dataqc_polytrendtest, dataqc_propogateflags, dataqc_solarelevation, dataqc_spiketest, dataqc_stuckvaluetest
 from data.ctd_functions import ctd_density, ctd_pracsal
 from data.adcp_functions import adcp_beam2ins, adcp_beam_eastward, adcp_beam_error, adcp_beam_northward, adcp_beam_vertical, adcp_ins2earth, adcp_magvar
-from data.data_functions import data_density
 
