2c3a14ef15232c3c31c05249899b92b622220429
Luke Campbell <luke.s.campbell@gmail.com>
2013-05-31 14:23:17 -0400

    Cython extension testing

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
+
 from setuptools import setup, find_packages
+from distutils.extension import Extension
+
+import numpy as np
+# setuptools DWIM monkey-patch madness
+# http://mail.python.org/pipermail/distutils-sig/2007-September/thread.html#8204
+import sys
+
+if 'setuptools.extension' in sys.modules:
+    m = sys.modules['setuptools.extension']
+    m.Extension.__dict__ = m._Extension.__dict__
+
 
 packages = find_packages()
 
@@ -11,6 +23,10 @@ Topic :: Scientific/Engineering
 Topic :: Education
 Topic :: Software Development :: Libraries :: Python Modules'''
 
+
+qc_extension = Extension("ion_functions.qc.qc_extensions", ["ion_functions/qc/qc_extensions.pyx"], include_dirs=[np.get_include()])
+
+
 setup(name = 'ion-functions', 
         version='0.0.1',
         description='Python Function collection for ION',
@@ -22,7 +38,8 @@ setup(name = 'ion-functions',
         classifiers=classifiers.split('\n'),
         packages=packages,
         keywords=['oceanography', 'seawater'],
-        setup_requires=[],
+        ext_modules=[qc_extension],
+        setup_requires=['setuptools_cython'],
         install_requires=[
             'ipython==0.13.0',
             'readline',
@@ -30,7 +47,8 @@ setup(name = 'ion-functions',
             'nose==1.1.2',
             'pygsw==0.0.9',
             'geomag==0.9',
-            'scipy==0.11.0'
+            'scipy==0.11.0',
+            'cython'
         ]
 )
 
