00ca10e6ab6019b2e334d34b1261b1f8f61d7bdf
Stuart Pearce <spearce@coas.oregonstate.edu>
2013-08-02 17:17:51 -0700

    updated the test data set for the metbk functions to be the same as the DPS

--- ion_functions/data/test/test_met_functions.py
+++ ion_functions/data/test/test_met_functions.py
@@ -10,6 +10,7 @@ from nose.plugins.attrib import attr
 from ion_functions.test.base_test import BaseUnitTestCase
 
 import numpy as np
+import datetime as dt
 
 from ion_functions.data.met_functions import windavg_mag_corr_east, windavg_mag_corr_north
 
@@ -18,43 +19,47 @@ from ion_functions.data.met_functions import windavg_mag_corr_east, windavg_mag_
 class TestMetFunctionsUnit(BaseUnitTestCase):
 
     def test_metbk_windavg(self):
-        lat = 14.6846
-        lon = -51.044
+        # Date, lat, and lon provided approximately equal magnetic
+        # declinations of -17 and +17 degrees
 
-        # timestamp in seconds since 1900-01-01
-        ts = np.array([
-            3319563600, 3319567200, 3319570800, 3319574400,
-            3319578000, 3319581600, 3319585200, 3319588800, 3319592400,
-            3319596000], dtype=np.float)
+        # have to convert these dates to ntp timestamp (seonds since
+        # 1900-01-01)
+        date_str = np.array([
+            '5/30/2013', '5/30/2013', '5/30/2013', '5/30/2013', '5/30/2013',
+            '5/30/2013', '5/30/2013', '5/30/2013'])
+        convert_to_ntp = lambda x: (
+            dt.datetime.strptime(x, '%m/%d/%Y') -
+            dt.datetime(1900, 1, 1)).total_seconds()
+        date_ts = map(convert_to_ntp, date_str)
 
-        # input velocities in cm/s
+        lat = np.array([
+            43.34, 43.34, 43.34, 43.34,
+            47.767, 47.767, 47.767, 47.767])
+        lon = np.array([
+            -66, -66, -66, -66,
+            -126, -126, -126, -126])
+
         ve = np.array([
-            -3.2,  0.1,  0., 2.3, -0.1,  5.6,  5.1,  5.8,
-            8.8, 10.3])
+            2.47, -2.47, -2.47, 2.47,
+            2.47, -2.47, -2.47, 2.47])
         vn = np.array([
-            18.2,  9.9, 12., 6.6, 7.4,  3.4, -2.6,  0.2,
-            -1.5,  4.1])
-        vu = np.array([-1.1, -0.6, -1.4, -2, -1.7, -2, 1.3, -1.6, -1.1, -4.5])
+            6.52, 6.52, -6.52, -6.52,
+            6.52, 6.52, -6.52, -6.52])
 
-        # convert from cm/s to m/s for metbk
-        ve /= 100.0
-        vn /= 100.0
-        vu /= 100.0
-
-        # expected output velocities in m/s
         ve_expected = np.array([
-            -0.085136, -0.028752, -0.036007, 0.002136, -0.023158,
-            0.043218, 0.056451, 0.054727, 0.088446, 0.085952])
+            0.46, -4.27, -0.46, 4.27,
+            4.27, -0.46, -4.27, 0.46])
         vn_expected = np.array([
-            0.164012,  0.094738,  0.114471,  0.06986,  0.07029,
-            0.049237, -0.009499,  0.019311,  0.012096,  0.070017])
-        vu_expected = np.array([
-            -0.011, -0.006, -0.014, -0.02, -0.017, -0.02,
-            0.013, -0.016, -0.011, -0.045])
-        ve_cor = windavg_mag_corr_east(ve, vn, lat, lon, ts, 6)
-        vn_cor = windavg_mag_corr_north(ve, vn, lat, lon, ts, 6)
-        vu_cor = vu
+            6.96, 5.51, -6.96, -5.51,
+            5.51, 6.96, -5.51, -6.96])
 
-        np.testing.assert_array_almost_equal(ve_cor, ve_expected)
-        np.testing.assert_array_almost_equal(vn_cor, vn_expected)
-        np.testing.assert_array_almost_equal(vu_cor, vu_expected)
+        ve_cor = windavg_mag_corr_east(ve, vn, lat, lon, date_ts)
+        vn_cor = windavg_mag_corr_north(ve, vn, lat, lon, date_ts)
+
+        # test data was only given to 2 decimals (despite that the
+        # function can calculate to better accuracy based on comparison
+        # of this function to the DPS Matlab function). So here the test
+        # data is only tested to 2 decimal places
+        np.testing.assert_array_almost_equal(ve_cor, ve_expected, decimal=2)
+        np.testing.assert_array_almost_equal(vn_cor, vn_expected, decimal=2)
+
