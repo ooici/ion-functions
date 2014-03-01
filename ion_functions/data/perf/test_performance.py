815de26e1612f2997ca582199f8b3c1f32a553b8
Christopher Wingard <cwingard@coas.oregonstate.edu>
2014-03-01 09:08:16 -0800

    Performance testing and improvements for CO2 functions

--- ion_functions/data/perf/test_performance.py
+++ ion_functions/data/perf/test_performance.py
@@ -8,23 +8,27 @@
 from nose.plugins.attrib import attr
 from unittest import TestCase
 
-
 import time
 import numpy as np
 
-a_year = 24 * 3600 * 365
-a_week = 24 * 3600 * 7
-a_month = 24 * 3600 * 30
-a_day = 24 * 3600
+a_deca = 10000                  # 10,000 values
+a_year = 60 * 60 * 24 * 365     # a years worth of 1 Hz data
+a_week = 60 * 60 * 24 * 7       # a weeks worth of 1 Hz data
+a_month = 60 * 60 * 24 * 30     # a months worth of 1 Hz data
+a_day = 60 * 60 * 24            # a days worth of 1 Hz data
+
 
 class TimeIt(object):
     def __init__(self, results=[]):
         self.results = results
+
     def __enter__(self):
         self.i = time.time()
+
     def __exit__(self, type, value, traceback):
         v = time.time() - self.i
         self.results.append(v)
+
 
 @attr('PERF')
 class PerformanceTestCase(TestCase):
@@ -32,7 +36,7 @@ class PerformanceTestCase(TestCase):
         for i in xrange(10):
             with TimeIt(stats):
                 func(*args, **kwargs)
-            print 'Run %i: %s' %(i,stats[i])
+            print 'Run %i: %s' % (i, stats[i])
             if stats[i] > 10:
                 raise AssertionError('Performance standard failed. Method exceeded 10 seconds')
 
