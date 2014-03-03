716a289ce94cef72323597db1f16f86953528d9d
Russell Desiderio <desi@coas.oregonstate.edu>
2014-03-03 10:12:03 -0800

    03-Mar-2014 1011

--- ion_functions/data/sfl_functions.py
+++ ion_functions/data/sfl_functions.py
@@ -5,28 +5,33 @@
 @author Christopher Wingard
 @brief Module containing Seafloor Properties related data-calculations.
 """
+
+import numpy as np
 from ion_functions.utils import fill_value
+
+
 def sfl_trhph_vfltemp(V_s, V_c, a, b, c, d, e):
     """
     Description:
-    
+
         OOI Level 1 Vent Fluid Temperature from TRHPH (TRHPHTE) data product,
         which is calculated using data from the Temperature Resistivity Probe
         (TRHPH) instrument.
-    
+
     Implemented by:
-    
+
         2013-05-01: Christopher Wingard. Initial Code
-    
+        2014-02-27: Russell Desiderio. Added documentation.
+
     Usage:
-    
-        T = sfl_trhph_vftemp(V_s, V_c, a, b, c, d, e)
-    
+
+        T = sfl_trhph_vfltemp(V_s, V_c, a, b, c, d, e)
+
             where
-    
-        T = Vent fluid temperature from TRHPH [deg_C]
-        V_c = Thermocouple voltage [volts]
-        V_s = Thermistor voltage [volts]
+
+        T = Vent fluid temperature from TRHPH (TRHPHTE_L1) [deg_C]
+        V_s = Thermistor voltage (TRHPHVS_L0) [volts]
+        V_c = Thermocouple voltage (TRHPHVC_L0) [volts]
         a = coefficient from 4th degree polynomial fit of laboratory
             calibration correction curve.
         b = coefficient from 4th degree polynomial fit of laboratory
@@ -37,9 +42,9 @@ def sfl_trhph_vfltemp(V_s, V_c, a, b, c, d, e):
             calibration correction curve.
         e = coefficient from 4th degree polynomial fit of laboratory
             calibration correction curve.
-    
+
     References:
-    
+
         OOI (2012). Data Product Specification for Vent Fluid Temperature from
             TRHPH. Document Control Number 1341-00150.
             https://alfresco.oceanobservatories.org/ (See: Company Home >> OOI
@@ -48,20 +53,95 @@ def sfl_trhph_vfltemp(V_s, V_c, a, b, c, d, e):
     """
     # raw thermistor temperature
     T_s = 27.50133 - 17.2658 * V_s + 15.83424 / V_s
-    
-    # raw thermocouple temperature
+
+    # raw thermocouple temperature (PD431)
     T_c = 244970. * V_c / 1000.
-    
+
     # uncorrected total temperature
     T_u = T_s + T_c
-    
+
     # correction based on laboratory calibration
     T_lc = a * T_u**4 + b * T_u**3 + c * T_u**2 + d * T_u + e 
-    
-    # final, corrected temperature at sensor tip    
+
+    # final, corrected temperature at sensor tip
     T = T_u + T_lc
-    
+
     return T
+
+
+def sfl_trhph_vfl_thermistor_temp(V_s):
+    """
+    Description:
+
+        Intermediate data product (not a core data product) requested by the authors
+        of the TRHPHTE DPS. It is the intstrument's raw thermistor temperature, useful
+        as an important instrument diagnostic. It is the same variable as T_s in the
+        function sfl_trhph_vfltemp.
+
+    Implemented by:
+
+        2014-02-28: Russell Desiderio. Initial Code
+
+    Usage:
+
+        T_s = sfl_trhph_vfltemp(V_s)
+
+            where
+
+        T_s = Thermistor reference temperature [deg_C]
+        V_s = Thermistor voltage (TRHPHVS_L0) [volts]
+
+    References:
+
+        OOI (2012). Data Product Specification for Vent Fluid Temperature from
+            TRHPH. Document Control Number 1341-00150.
+            https://alfresco.oceanobservatories.org/ (See: Company Home >> OOI
+            >> Controlled >> 1000 System Level >>
+            1341-00150_Data_Product_SPEC_TRHPHTE_OOI.pdf)
+    """
+    # raw thermistor temperature
+    T_s = 27.50133 - 17.2658 * V_s + 15.83424 / V_s
+
+    return T_s
+
+
+def sfl_trhph_vflorp(V, offset, gain):
+    """
+    Description:
+
+        OOI Level 1 Vent Fluid Oxidation-Reduction Potential (ORP) from TRHPH
+        (TRHPHEH) data product, which is calculated using data from the Resistivity-
+        Temperature Probe (TRHPH) instrument.
+
+    Implemented by:
+
+        2014-02-28: Russell Desiderio. Initial Code
+
+    Usage:
+
+        ORP = sfl_trhph_vflorp(V)
+
+            where
+
+        ORP = Oxidation-Reduction Potential (TRHPHEH_L1) [mV]
+        V = ORP sensor voltage (TRHPHVO_L0) [volts]
+        offset = calibration coefficient; electronic offset [mV]
+        gain = calibration coefficient; gain multiplier [unitless]
+
+    References:
+
+        OOI (2012). Data Product Specification for Vent Fluid Temperature from
+            TRHPH. Document Control Number 1341-00150.
+            https://alfresco.oceanobservatories.org/ (See: Company Home >> OOI
+            >> Controlled >> 1000 System Level >>
+            1341-00150_Data_Product_SPEC_TRHPHTE_OOI.pdf)
+    """
+    # convert sensor voltage V from volts to mV;
+    # subtract offset;
+    # undo gain multiplier.
+    ORP = (V * 1000.0 - offset)/gain
+
+    return ORP
 
 
 def sfl_trhph_chlorconc(V_R1, V_R2, V_R3, T):
@@ -69,83 +149,93 @@ def sfl_trhph_chlorconc(V_R1, V_R2, V_R3, T):
     Wrapper function to vectorize the vent fluid chloride calculation defined
     below in sfl_trhph_chloride.
     """
-    import numpy as np
-    
+
     sfunc = np.vectorize(sfl_trhph_chloride)
     Cl = sfunc(V_R1, V_R2, V_R3, T)
-    
+
     return Cl
 
 
 def sfl_trhph_chloride(V_R1, V_R2, V_R3, T):
     """
     Description:
-    
+
         OOI Level 1 Vent Fluid Chloride Concentration from TRHPH (TRHPHCC) data
         product, which is calculated using data from the Temperature
         Resistivity Probe (TRHPH) instrument.
-    
+
     Implemented by:
-    
+
         2013-05-01: Christopher Wingard. Initial Code
-    
+        2014-02-28: Russell Desiderio. Modified code to better handle nans and fill_values.
+                                       Added more documentation to algorithm.
+
     Usage:
-    
+
         Cl = sfl_trhph_chloride(V_R1, V_R2, V_R3, T)
-    
+
             where
-    
+
         Cl = Vent fluid chloride concentration from TRHPH [mmol kg-1]
-        V_R1 = Resistivity voltage 1 (TRHPHR1) [volts]
-        V_R2 = Resistivity voltage 1 (TRHPHR2) [volts]
-        V_R3 = Resistivity voltage 1 (TRHPHR3) [volts]
-        T = Vent fluid temperature from TRHPH (TRHPHTE) [deg_C]
-    
+        V_R1 = Resistivity voltage 1 (TRHPHR1_L0) [volts]
+        V_R2 = Resistivity voltage 1 (TRHPHR2_L0) [volts]
+        V_R3 = Resistivity voltage 1 (TRHPHR3_L0) [volts]
+        T = Vent fluid temperature from TRHPH (TRHPHTE_L1) [deg_C]
+
     References:
-    
+
         OOI (2012). Data Product Specification for Vent Fluid Temperature from
             TRHPH. Document Control Number 1341-00150.
             https://alfresco.oceanobservatories.org/ (See: Company Home >> OOI
             >> Controlled >> 1000 System Level >>
             1341-00150_Data_Product_SPEC_TRHPHTE_OOI.pdf)
     """
-    import numpy as np
     from scipy.interpolate import RectBivariateSpline
-    
+
     # load sfl_functions_surface.py This loads the 3-dimensional calibration
-    # surface of temperature, conductivity and chloride reproduced as numpy
+    # surface of temperature, chloride, and conductivity reproduced as numpy
     # arrays from Larson_2007surface.mat
     from ion_functions.data.sfl_functions_surface import tdat, sdat, cdat
-    
+
     # select the optimal L0 Resistivity voltage
     V_R = V_R1 * 5.              # Option 1, default (V_R1 * 5)
-    
-    vflag = np.where(V_R2 < 0.75)         # Option 2 
+
+    vflag = np.where(V_R2 < 0.75)         # Option 2
     V_R[vflag] = V_R3[vflag] / 5.
-    
+
     vflag = np.where((V_R2 >= 0.75) & (V_R2 < 3.90))    # Option 3
     V_R[vflag] = V_R2[vflag]
-    
+
     # convert resistivity to conductivity
     C = 1. / V_R
-    
-    # extract curves of constant temperature out of the data surfaces
-    Cl = np.zeros(len(C))
+
+    # initialize product array Cl [mmol/kg] as nan values
+    Cl = np.zeros(len(C)) * np.nan
+    # set up chloride [S, mol/kg] range
     Scurve = np.linspace(np.min(sdat), np.max(sdat), 100,
                          endpoint='True')
+    # create bivariate spline for interpolation
     f = RectBivariateSpline(tdat, sdat, cdat.T, kx=1, ky=1, s=0)
     for i in range(len(Cl)):
+        # constant T vector
         Tcurve = np.zeros(len(Scurve)) + T[i]
+        # find conductivity curve Ccurve at constant T as a f(chloride)
         Ccurve = f(Tcurve, Scurve)
+        # even when T is out-of-range, the procedure will give finite values for S.
+        # therefore, this case must be specifically handled in the conditional.
         if (np.all(np.isfinite(Ccurve)) and
-            (T[i] >= min(tdat) and T[i] <= max(tdat))):
-            #now interpolate onto the Scurve/Ccurve
-            S = np.interp(C[i], Ccurve[0,:], Scurve, left=np.nan, right=np.nan)
-            Cl[i] = np.round(S * 1000.)
-        else:
+           (T[i] >= min(tdat) and T[i] <= max(tdat))):
+            # now interpolate measured conductivity C into (Ccurve,Scurve)
+            S = np.interp(C[i], Ccurve[0, :], Scurve, left=np.nan, right=np.nan)
+            Cl[i] = np.round(S * 1000.)  # change units
+        # the following statement handles the two cases:
+        # (1) the conditional was executed, resulting in a nan value.
+        # (2) the conditional was not executed, therefore the initialized value is nan.
+        if np.isnan(Cl[i]):
             Cl[i] = fill_value
-    
+
     return Cl
+
 
 import numexpr as ne
 
--- ion_functions/data/test/test_sfl_functions.py
+++ ion_functions/data/test/test_sfl_functions.py
@@ -14,6 +14,7 @@ import numpy as np
 from ion_functions.data import sfl_functions as sflfunc
 from ion_functions.utils import fill_value
 
+
 @attr('UNIT', group='func')
 class TestSFLFunctionsUnit(BaseUnitTestCase):
 
@@ -22,16 +23,16 @@ class TestSFLFunctionsUnit(BaseUnitTestCase):
         Test sfl_trhph_vfltemp function.
 
         Values based on those described in DPS as available on Alfresco:
-        
+
         OOI (2012). Data Product Specification for Vent Fluid Temperature from
             TRHPH. Document Control Number 1341-00150.
             https://alfresco.oceanobservatories.org/ (See: Company Home >> OOI
             >> Controlled >> 1000 System Level >>
             1341-00150_Data_Product_SPEC_TRHPHTE_OOI.pdf)
-            
+
         Implemented by Christopher Wingard, April 2013
         """
-        
+
         # calibration constants
         a = 1.98e-9
         b = -2.45e-6
@@ -39,6 +40,7 @@ class TestSFLFunctionsUnit(BaseUnitTestCase):
         d = -0.0888
         e = 0.731
 
+        #   [  V_s,   V_c,   T_s,  T_c,  T_u,  T_lc,     T]
         test_array = np.array([
             [1.506, 0.000, 12.01, 0.00, 12.0, -0.206, 11.8],
             [1.479, 0.015, 12.67, 3.68, 16.3, -0.483, 15.9],
@@ -55,67 +57,193 @@ class TestSFLFunctionsUnit(BaseUnitTestCase):
             [1.928, 1.319, 2.43, 323.12, 325.5, 7.883, 333.4],
             [1.929, 1.318, 2.40, 322.87, 325.3, 7.880, 333.2]
         ])
-        
-        # set inputs
-        V_s = test_array[:,0]
-        V_c = test_array[:,1]
 
-        # set known output
-        T = test_array[:,6]
-        
+        # set inputs
+        V_s = test_array[:, 0]
+        V_c = test_array[:, 1]
+
+        # set known outputs
+        T = test_array[:, 6]
+
         # calculate the Vent Fluid Temperature
         Tout = sflfunc.sfl_trhph_vfltemp(V_s, V_c, a, b, c, d, e)
-        
-        # How'd we do?
-        np.testing.assert_allclose(Tout, T, rtol=0.1, atol=0)
 
+        # How'd we do?
+        np.testing.assert_allclose(Tout, T, rtol=0.01, atol=0.0)
+
+    def test_sfl_trhph_vfl_thermistor_temp(self):
+        """
+        Test sfl_trhph_vfl_thermistor_temp function.
+
+        Values based on those described in DPS as available on Alfresco:
+
+        OOI (2012). Data Product Specification for Vent Fluid Temperature from
+            TRHPH. Document Control Number 1341-00150.
+            https://alfresco.oceanobservatories.org/ (See: Company Home >> OOI
+            >> Controlled >> 1000 System Level >>
+            1341-00150_Data_Product_SPEC_TRHPHTE_OOI.pdf)
+
+        Implemented by Russell Desiderio, 28-Feb-2014
+        """
+
+        #   [  V_s,   V_c,   T_s,  T_c,  T_u,  T_lc,     T]
+        test_array = np.array([
+            [1.506, 0.000, 12.01, 0.00, 12.0, -0.206, 11.8],
+            [1.479, 0.015, 12.67, 3.68, 16.3, -0.483, 15.9],
+            [1.926, 0.001, 2.47, 0.25, 2.7, 0.497, 3.2],
+            [1.932, 0.274, 2.34, 67.12, 69.5, -1.735, 67.7],
+            [1.927, 0.306, 2.45, 74.96, 77.4, -1.648, 75.8],
+            [1.930, 0.393, 2.38, 96.27, 98.7, -1.162, 97.5],
+            [1.929, 0.383, 2.40, 93.82, 96.2, -1.234, 95.0],
+            [1.930, 0.388, 2.38, 95.05, 97.4, -1.199, 96.2],
+            [1.930, 0.469, 2.38, 114.89, 117.3, -0.497, 116.8],
+            [1.931, 1.077, 2.36, 263.83, 266.2, 6.579, 272.8],
+            [1.926, 1.288, 2.47, 315.52, 318.0, 7.797, 325.8],
+            [1.926, 1.305, 2.47, 319.69, 322.2, 7.847, 330.0],
+            [1.928, 1.319, 2.43, 323.12, 325.5, 7.883, 333.4],
+            [1.929, 1.318, 2.40, 322.87, 325.3, 7.880, 333.2]
+        ])
+
+        # set inputs
+        V_s = test_array[:, 0]
+
+        # set known outputs
+        T_s = test_array[:, 2]
+
+        # calculate the thermistor temperature
+        Tout = sflfunc.sfl_trhph_vfl_thermistor_temp(V_s)
+
+        # How'd we do?
+        np.testing.assert_allclose(Tout, T_s, rtol=0.01, atol=0.0)
+
+    def test_sfl_trhph_vflorp(self):
+        """
+        Test sfl_trhph_vflorp function.
+
+        Values based on those described in DPS as available on Alfresco:
+
+        OOI (2012). Data Product Specification for Vent Fluid Temperature from
+            TRHPH. Document Control Number 1341-00150.
+            https://alfresco.oceanobservatories.org/ (See: Company Home >> OOI
+            >> Controlled >> 1000 System Level >>
+            1341-00150_Data_Product_SPEC_TRHPHTE_OOI.pdf)
+
+        Implemented by Russell Desiderio, 28-Feb-2014
+        """
+
+        # test calibration constants
+        offset = 2004.0
+        gain = 4.0
+
+        #   [  V      ORP[mV] ]
+        test_array = np.array([
+            [1.806,   -50],
+            [1.541,  -116],
+            [1.810,   -49],
+            [0.735,  -317],
+            [0.745,  -315],
+            [0.715,  -322],
+            [0.775,  -307],
+            [0.799,  -301],
+            [0.757,  -312],
+            [0.542,  -366],
+            [0.831,  -293],
+            [0.867,  -284],
+            [0.911,  -273]
+        ])
+
+        # set inputs
+        V = test_array[:, 0]
+
+        # set known outputs
+        ORP = test_array[:, -1]
+
+        # calculate the oxidation-reduction potential
+        ORP_out = sflfunc.sfl_trhph_vflorp(V, offset, gain)
+        ORP_out = np.round(ORP_out)
+
+        # How'd we do?
+        np.testing.assert_allclose(ORP_out, ORP, rtol=0.01, atol=0.0)
 
     def test_sfl_trhph_chloride(self):
         """
         Test sfl_trhph_chloride function.
 
         Values based on those described in DPS as available on Alfresco:
-        
+
         OOI (2012). Data Product Specification for Vent Fluid Chloride
             Concentration. Document Control Number 1341-00160.
             https://alfresco.oceanobservatories.org/ (See: Company Home >> OOI
             >> Controlled >> 1000 System Level >>
             1341-00150_Data_Product_SPEC_TRHPHTE_OOI.pdf)
-            
+
         Implemented by Christopher Wingard, April 2013
+        Modified by Russell Desiderio, Feb. 28, 2014.
+            Extended unit tests; see comments below.
         """
+        # original unit test data from DPS:
+        #    did not test v_r1 v_r2 v_r3 conditional.
+        #    did not unambiguously test resistivity value out of range.
+        #        (all of the test result fill_values could arise
+        #         solely from temperature out-of-range)
+        #test_array = np.array([
+        #    [0.906, 4.095, 4.095, 11.8, 4.530, 0.2208, fill_value, fill_value],
+        #    [0.890, 4.095, 4.095, 15.9, 4.450, 0.2247, fill_value, fill_value],
+        #    [0.891, 4.095, 4.095, 3.2, 4.455, 0.2245, fill_value, fill_value],
+        #    [0.184, 0.915, 4.064, 67.7, 0.915, 1.0929, fill_value, fill_value],
+        #    [0.198, 1.002, 4.095, 75.8, 1.002, 0.9980, fill_value, fill_value],
+        #    [0.172, 0.857, 4.082, 97.5, 0.857, 1.1669, fill_value, fill_value],
+        #    [0.183, 0.926, 4.076, 95.0, 0.926, 1.0799, fill_value, fill_value],
+        #    [0.233, 1.182, 4.072, 96.2, 1.182, 0.8460, fill_value, fill_value],
+        #    [0.146, 0.747, 3.634, 116.8, 0.727, 1.3759, 0.19507, 195],
+        #    [0.134, 0.681, 3.405, 272.8, 0.681, 1.4684, 0.10893, 109],
+        #    [0.131, 0.673, 3.293, 325.8, 0.659, 1.5184, 0.12813, 128],
+        #    [0.133, 0.678, 3.396, 330.0, 0.679, 1.4723, 0.12664, 127],
+        #    [0.135, 0.681, 3.409, 333.4, 0.682, 1.4667, 0.12878, 129],
+        #    [0.135, 0.681, 3.426, 333.2, 0.685, 1.4594, 0.12802, 128]
+        #])
+
+        # the first fill_value tests out-of-range resistivity value v_r1 which
+        #     results in an out-of-range conductivity value given the curvature
+        #     of the surface as a function of temperature; for other temperatures,
+        #     this v_r1 value (0.380) is not out-of-range (cf 1st and 3rd rows).
+        # the second fill_value tests an out-of-range temperature value (84.6).
+        # the third fill_value tests an out_of_range resistivity value (v_r2=2.8).
+        #
+        #     v_r1    v_r2    v_r3   temp    chl [mmol/kg]
         test_array = np.array([
-            [0.906, 4.095, 4.095, 11.8, 4.530, 0.2208, fill_value, fill_value],
-            [0.890, 4.095, 4.095, 15.9, 4.450, 0.2247, fill_value, fill_value],
-            [0.891, 4.095, 4.095, 3.2, 4.455, 0.2245, fill_value, fill_value],
-            [0.184, 0.915, 4.064, 67.7, 0.915, 1.0929, fill_value, fill_value],
-            [0.198, 1.002, 4.095, 75.8, 1.002, 0.9980, fill_value, fill_value],
-            [0.172, 0.857, 4.082, 97.5, 0.857, 1.1669, fill_value, fill_value],
-            [0.183, 0.926, 4.076, 95.0, 0.926, 1.0799, fill_value, fill_value],
-            [0.233, 1.182, 4.072, 96.2, 1.182, 0.8460, fill_value, fill_value],
-            [0.146, 0.747, 3.634, 116.8, 0.727, 1.3759, 0.19507, 195],
-            [0.134, 0.681, 3.405, 272.8, 0.681, 1.4684, 0.10893, 109],
-            [0.131, 0.673, 3.293, 325.8, 0.659, 1.5184, 0.12813, 128],
-            [0.133, 0.678, 3.396, 330.0, 0.679, 1.4723, 0.12664, 127],
-            [0.135, 0.681, 3.409, 333.4, 0.682, 1.4667, 0.12878, 129],
-            [0.135, 0.681, 3.426, 333.2, 0.685, 1.4594, 0.12802, 128]
+            [0.440,  4.095,  4.095,  105.4,    59.0],
+            [0.380,  4.095,  4.095,  241.9,   fill_value],
+            [0.320,  4.095,  4.095,  374.2,    60.0],
+            [0.184,  0.915,  4.064,  105.4,   175.0],
+            [0.198,  1.002,  4.095,  241.9,    71.0],
+            [0.172,  0.857,  4.082,  374.2,   132.0],
+            [0.183,  0.926,  4.076,   84.6,   fill_value],
+            [0.233,  2.800,  4.072,  250.2,   fill_value],
+            [0.146,  0.747,  3.634,  116.8,   195.0],
+            [0.134,  0.681,  3.405,  272.8,   109.0],
+            [0.131,  0.673,  3.293,  325.8,   128.0],
+            [0.133,  0.678,  3.396,  330.0,   127.0],
+            [0.135,  0.681,  2.000,  333.4,   239.0],
+            [0.135,  0.681,  1.000,  333.2,   501.0]
         ])
-        
+
         # set inputs
-        V_R1 = test_array[:,0]
-        V_R2 = test_array[:,1]
-        V_R3 = test_array[:,2]
-        T = test_array[:,3]
+        V_R1 = test_array[:, 0]
+        V_R2 = test_array[:, 1]
+        V_R3 = test_array[:, 2]
+        T = test_array[:, 3]
 
         # set known output
-        Cl = test_array[:,7]
-        
+        Cl = test_array[:, -1]
+
         # calculate the Vent Fluid Temperature
         Clout = sflfunc.sfl_trhph_chloride(V_R1, V_R2, V_R3, T)
-        
+
+        ###########################################################################
         # How'd we do?
         np.testing.assert_allclose(Clout, Cl, rtol=0, atol=0)
-
+        ###########################################################################
 
     def test_sfl_sflpres_l1(self):
                 """
