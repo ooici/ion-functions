3db879f1813a1e05b4ce5d2fe17abaf17ddc2e06
Russell Desiderio <desi@coas.oregonstate.edu>
2014-04-04 14:15:22 -0700

    adcp functions - improved performance by replacing forloops with np.einsum

--- ion_functions/data/adcp_functions.py
+++ ion_functions/data/adcp_functions.py
@@ -3,10 +3,10 @@
 @package ion_functions.data.adcp_functions
 @file ion_functions/data/adcp_functions.py
 @author Christopher Wingard
-@brief Module containing CTD related data-calculations.
+@brief Module containing ADCP related data-calculations.
 """
 import numpy as np
-from ion_functions.data.generic_functions import magnetic_declination, magnetic_correction
+from ion_functions.data.generic_functions import magnetic_declination
 
 
 # Wrapper functions to create 1:1 outputs for ParameterFunctions in Preload
@@ -23,7 +23,11 @@ def adcp_beam_eastward(b1, b2, b3, b4, h, p, r, vf, lat, lon, z, dt):
 
         2013-04-10: Christopher Wingard. Initial code.
         2014-02-03: Christopher Wingard. Formatting and adjusting to use
-                    magnetic declination values calulated use the WMM 2010.
+                    magnetic declination values calculated use the WMM 2010.
+        2014-04-04: Russell Desiderio. Optimized code performance by replacing the
+                    for loops previously used to calculate 2D and 3D vectorized
+                    coordinate transformations with calls to np.einsum (numpy
+                    Einstein summation function).
 
     Usage:
 
@@ -32,7 +36,7 @@ def adcp_beam_eastward(b1, b2, b3, b4, h, p, r, vf, lat, lon, z, dt):
             where
 
         uu_corr = east velocity profiles in Earth coordinates corrected for the
-                  magnetic declination (VELPROF-VLE_L1) [mm s-1]
+                  magnetic declination (VELPROF-VLE_L1) [m s-1]
 
         b1 = "beam 1" velocity profiles in beam coordinates (VELPROF-B1_L0) [mm s-1]
         b2 = "beam 2" velocity profiles in beam coordinates (VELPROF-B2_L0) [mm s-1]
@@ -65,20 +69,16 @@ def adcp_beam_eastward(b1, b2, b3, b4, h, p, r, vf, lat, lon, z, dt):
     # compute the beam to instrument transform
     u, v, w, e = adcp_beam2ins(b1, b2, b3, b4)
 
-    # calculate the magnetic declination using the WWM2010 model
+    # compute the instrument to earth beam transform
+    uu, vv, _ = adcp_ins2earth(u, v, w, h, p, r, vf)
+
+    # compute the magnetic variation, and
     theta = magnetic_declination(lat, lon, dt, z)
 
-    # iterate through arrays for processing multiple records
-    uu_cor = np.empty((b1.shape))
-    for indx in range(b1.shape[0]):
-        # compute the instrument to earth beam transform
-        uu, vv, ww = adcp_ins2earth(u[indx, :], v[indx, :], w[indx, :],
-                                    h[indx], p[indx], r[indx], vf[indx])
+    # correct for it
+    uu_cor, _ = magnetic_correction_vctrzd(theta, uu, vv)
 
-        # apply the magnetic variation correction
-        uu_cor[indx, :], vcor = magnetic_correction(theta[indx], uu, vv)
-
-    # scale eastward velocity to m/s
+    # scale velocity to m/s
     uu_cor = uu_cor / 1000.  # mm/s -> m/s
 
     # return the Eastward Velocity Profile
@@ -89,7 +89,7 @@ def adcp_beam_northward(b1, b2, b3, b4, h, p, r, vf, lat, lon, z, dt):
     """
     Description:
 
-        Wrapper function to compute the Eastward Velocity Profile (VELPROF-VLN)
+        Wrapper function to compute the Northward Velocity Profile (VELPROF-VLN)
         from beam coordinate transformed velocity profiles as defined in the
         Data Product Specification for Velocity Profile and Echo Intensity -
         DCN 1341-00750.
@@ -98,16 +98,21 @@ def adcp_beam_northward(b1, b2, b3, b4, h, p, r, vf, lat, lon, z, dt):
 
         2013-04-10: Christopher Wingard. Initial code.
         2014-02-03: Christopher Wingard. Formatting and adjusting to use
-                    magnetic declination values calulated use the WMM 2010.
+                    magnetic declination values calculated use the WMM 2010.
+        2014-03-28: Russell Desiderio. Corrected documentation only (east->north).
+        2014-04-04: Russell Desiderio. Optimized code performance by replacing the
+                    for loops previously used to calculate 2D and 3D vectorized
+                    coordinate transformations with calls to np.einsum (numpy
+                    Einstein summation function).
 
     Usage:
 
-        uu_cor = adcp_beam_eastward(b1, b2, b3, b4, h, p, r, vf, lat, lon, z, dt)
+        vv_cor = adcp_beam_northward(b1, b2, b3, b4, h, p, r, vf, lat, lon, z, dt)
 
             where
 
-        uu_corr = east velocity profiles in Earth coordinates corrected for the
-                  magnetic declination (VELPROF-VLE_L1) [mm s-1]
+        vv_corr = north velocity profiles in Earth coordinates corrected for the
+                  magnetic declination (VELPROF-VLN_L1) [m s-1]
 
         b1 = "beam 1" velocity profiles in beam coordinates (VELPROF-B1_L0) [mm s-1]
         b2 = "beam 2" velocity profiles in beam coordinates (VELPROF-B2_L0) [mm s-1]
@@ -140,20 +145,16 @@ def adcp_beam_northward(b1, b2, b3, b4, h, p, r, vf, lat, lon, z, dt):
     # compute the beam to instrument transform
     u, v, w, e = adcp_beam2ins(b1, b2, b3, b4)
 
-    # calculate the magnetic declination using the WWM2010 model
+    # compute the instrument to earth beam transform
+    uu, vv, _ = adcp_ins2earth(u, v, w, h, p, r, vf)
+
+    # compute the magnetic variation, and
     theta = magnetic_declination(lat, lon, dt, z)
 
-    # iterate through arrays for processing multiple records
-    vv_cor = np.empty((b1.shape))
-    for indx in range(b1.shape[0]):
-        # compute the instrument to earth beam transform
-        uu, vv, ww = adcp_ins2earth(u[indx, :], v[indx, :], w[indx, :],
-                                    h[indx], p[indx], r[indx], vf[indx])
+    # corect for it
+    _, vv_cor = magnetic_correction_vctrzd(theta, uu, vv)
 
-        # apply the magnetic variation correction
-        ucor, vv_cor[indx, :] = magnetic_correction(theta[indx], uu, vv)
-
-    # scale northward velocity to m/s
+    # scale velocity to m/s
     vv_cor = vv_cor / 1000.  # mm/s -> m/s
 
     # return the Northward Velocity Profile
@@ -178,17 +179,13 @@ def adcp_beam_vertical(b1, b2, b3, b4, h, p, r, vf):
     # compute the beam to instrument transform
     u, v, w, e = adcp_beam2ins(b1, b2, b3, b4)
 
-    # iterate through arrays for processing multiple records
-    ww = np.empty((b1.shape))
-    for indx in range(b1.shape[0]):
-        # compute the instrument to earth beam transform
-        uu, vv, ww[indx, :] = adcp_ins2earth(u[indx, :], v[indx, :], w[indx, :],
-                                             h[indx], p[indx], r[indx], vf[indx])
+    # compute the instrument to earth beam transform
+    _, _, ww = adcp_ins2earth(u, v, w, h, p, r, vf)
 
-    # scale eastward velocity to m/s
+    # scale upward velocity to m/s
     ww = ww / 1000.  # mm/s -> m/s
 
-    # return the Eastward Velocity Profile
+    # return the Upward Velocity Profile
     return ww
 
 
@@ -217,6 +214,10 @@ def adcp_earth_eastward(u, v, z, lat, lon, dt):
     """
     Wrapper function to compute the Eastward Velocity Profile (VELPROF-VLE)
     from the Earth coordinate transformed data.
+
+    This function was written to test intermediate data product values, and
+    is not used to calculate the final data products. However, it is used
+    in the unit tests, and so should not be deleted.
     """
     # force shapes of inputs to arrays
     u = np.atleast_2d(u)
@@ -229,13 +230,10 @@ def adcp_earth_eastward(u, v, z, lat, lon, dt):
     # calculate the magnetic declination using the WWM2010 model
     theta = magnetic_declination(lat, lon, dt, z)
 
-    # iterate through arrays for processing multiple records
-    uu_cor = np.empty(u.shape)
-    for indx in range(u.shape[0]):
-        # apply the magnetic variation correction
-        uu_cor[indx, :], vcor = magnetic_correction(theta[indx], u[indx, :], v[indx, :])
+    # correct u and v for magnetic declination
+    uu_cor, _ = magnetic_correction_vctrzd(theta, u, v)
 
-    # scale eastward velocity to m/s
+    # scale velocity to m/s
     uu_cor = uu_cor / 1000.  # mm/s -> m/s
 
     # return the Eastward Velocity Profile
@@ -246,8 +244,12 @@ def adcp_earth_northward(u, v, z, lat, lon, dt):
     """
     Wrapper function to compute the Northward Velocity Profile (VELPROF-VLN)
     from the Earth coordinate transformed data.
+
+    This function was written to test intermediate data product values, and
+    is not used to calculate the final data products. However, it is used
+    in the unit tests, and so should not be deleted.
     """
-    # force shapes of inputs to arrays of the correct dimensions
+    # force shapes of inputs to arrays
     u = np.atleast_2d(u)
     v = np.atleast_2d(v)
     z = np.atleast_1d(z) / 10.  # scale decimeter depth input to meters
@@ -258,13 +260,10 @@ def adcp_earth_northward(u, v, z, lat, lon, dt):
     # calculate the magnetic declination using the WWM2010 model
     theta = magnetic_declination(lat, lon, dt, z)
 
-    # iterate through arrays for processing multiple records
-    vv_cor = np.empty(u.shape)
-    for indx in range(u.shape[0]):
-        # apply the magnetic variation correction
-        ucor, vv_cor[indx, :] = magnetic_correction(theta[indx], u[indx, :], v[indx, :])
+    # correct u and v for magnetic declination
+    _, vv_cor = magnetic_correction_vctrzd(theta, u, v)
 
-    # scale northward velocity to m/s
+    # scale velocity to m/s
     vv_cor = vv_cor / 1000.  # mm/s -> m/s
 
     # return the Northward Velocity Profile
@@ -375,6 +374,9 @@ def adcp_ins2earth(u, v, w, heading, pitch, roll, vertical):
     Implemented by:
 
         2013-04-10: Christopher Wingard. Initial code.
+        2014-04-04: Russell Desiderio. Optimized code performance by replacing the for
+                    loops previously used to calculate vectorized matrix multiplication
+                    products with calls to np.einsum (numpy Einstein summation function).
 
     Usage:
 
@@ -403,38 +405,172 @@ def adcp_ins2earth(u, v, w, heading, pitch, roll, vertical):
             >> Controlled >> 1000 System Level >>
             1341-00050_Data_Product_SPEC_VELPROF_OOI.pdf)
     """
-    # insure we are dealing with array inputs and determine the number of bins
-    # for output arrays
-    u = np.atleast_1d(u)
-    v = np.atleast_1d(v)
-    w = np.atleast_1d(w)
+    # insure we are dealing with array inputs
+    u = np.atleast_2d(u)
+    v = np.atleast_2d(v)
+    w = np.atleast_2d(w)
+
+    heading = np.atleast_1d(heading)
+    pitch = np.atleast_1d(pitch)
+    roll = np.atleast_1d(roll)
+    vertical = np.atleast_1d(vertical)
 
     # if the unit is oriented looking up, add 180 degrees
     mask = (vertical == 1)
     R = roll + (180.0 * mask)
 
+    # roll
     Rrad = np.radians(R)
+    cos_R = np.cos(Rrad)
+    sin_R = np.sin(Rrad)
+    # heading
     Hrad = np.radians(heading)
+    cos_H = np.cos(Hrad)
+    sin_H = np.sin(Hrad)
+    # pitch
     t1rad = np.radians(pitch)
     t2rad = np.radians(roll)
-    P = np.arctan(np.tan(t1rad) * np.cos(t2rad))
+    Prad = np.arctan(np.tan(t1rad) * np.cos(t2rad))
+    cos_P = np.cos(Prad)
+    sin_P = np.sin(Prad)
 
-    # create the multiplication matrix
-    M1 = np.matrix([
-        [np.cos(Hrad), np.sin(Hrad), 0.0],
-        [-np.sin(Hrad), np.cos(Hrad), 0.0],
-        [0.0, 0.0, 1.0]])
-    M2 = np.matrix([
-        [1.0, 0.0, 0.0],
-        [0.0, np.cos(P), -np.sin(P)],
-        [0.0, np.sin(P), np.cos(P)]])
-    M3 = np.matrix([
-        [np.cos(Rrad), 0.0, np.sin(Rrad)],
-        [0.0, 1.0, 0.0],
-        [-np.sin(Rrad), 0.0, np.cos(Rrad)]])
-    M = M1 * M2 * M3
+    # determine array size
+    n_packets = u.shape[0]
+    n_uvw = u.shape[1]
 
-    # apply the Earth transform for each instrument velocity bin
-    uu, vv, ww = np.array(np.dot(M, np.array([u, v, w])))
+    # initialize vectors to be used as matrix elements
+    ones = np.ones(n_packets)
+    zeros = ones * 0.0
+
+    # the rollaxis calls reorient the matrices so that their lead index is
+    # the data packet index
+    M1 = np.array([[cos_H, sin_H, zeros],
+                   [-sin_H, cos_H, zeros],
+                   [zeros, zeros, ones]])
+    M1 = np.rollaxis(M1, 2)
+    M2 = np.array([[ones, zeros, zeros],
+                   [zeros, cos_P, -sin_P],
+                   [zeros, sin_P, cos_P]])
+    M2 = np.rollaxis(M2, 2)
+    M3 = np.array([[cos_R, zeros, sin_R],
+                   [zeros, ones, zeros],
+                   [-sin_R, zeros, cos_R]])
+    M3 = np.rollaxis(M3, 2)
+
+    # construct input array of coordinates (velocities) to be transformed.
+    # the basis set is 3D (E,N,U) so that the middle dimension is sized at 3.
+    uvw = np.zeros((n_packets, 3, n_uvw))
+
+    # pack the coordinates (velocities) to be transformed into the appropriate slices.
+    uvw[:, 0, :] = u
+    uvw[:, 1, :] = v
+    uvw[:, 2, :] = w
+
+    # the Einstein summation is here configured to do the matrix
+    # multiplication MM(i,l) = M1(i,j) * M2(j,k) * M3(k,l) on each slice h.
+    MM = np.einsum('hij,hjk,hkl->hil', M1, M2, M3)
+
+    # the Einstein summation is here configured to do the matrix
+    # multiplication uvw_earth(i,m) = MM(i,l) * uvw(l,m) on each slice h.
+    uvw_earth = np.einsum('hil,hlm->him', MM, uvw)
+
+    # NOTE:
+    # these last two executable statements run about a factor of 2
+    # faster in the 10000 data packet performance tests versus combining
+    # these operations into the one statement:
+    #     uvw_earth = np.einsum('hij,hjk,hkl,hlm->him', M1, M2, M3, uvw)
+
+    # break out the coordinate slices and return them
+    uu = uvw_earth[:, 0, :]
+    vv = uvw_earth[:, 1, :]
+    ww = uvw_earth[:, 2, :]
 
     return (uu, vv, ww)
+
+
+def magnetic_correction_vctrzd(theta, u, v):
+    """
+    Description:
+
+        This function corrects velocity profiles for the magnetic variation
+        (declination) at the measurement location.  The magnetic declination
+        is obtained from the 2010 World Magnetic Model (WMM2010) provided by
+        NOAA (see wmm_declination).
+
+        This version handles 'vectorized' input variables without using for
+        loops. It was specifically written to handle the case of a 1D array of
+        theta values, theta=f(i), with corresponding sets of 'u' and 'v' values
+        such that u=f(i,j) and v=f(i,j), where there are j 'u' and 'v' values
+        for each theta(i).
+
+    Implemented by:
+
+        2014-04-04: Russell Desiderio. Initial code. This function is used to
+                    calculate magnetic corrections by the functions contained
+                    in this module instead of the function magnetic_correction
+                    found in ion_functions.data.generic_functions.
+
+    Usage:
+
+        u_cor, v_cor = magnetic_correction(theta, u, v)
+
+            where
+
+        u_cor = eastward velocity profiles, in earth coordinates, with
+            the correction for magnetic variation applied.
+        v_cor = northward velocity profiles, in earth coordinates,
+            with the correction for magnetic variation applied.
+
+        theta = magnetic variation based on location (latitude, longitude and
+            altitude) and date; units of theta are [degrees]
+        u = uncorrected eastward velocity profiles in earth coordinates
+        v = uncorrected northward velocity profiles in earth coordinates
+
+    References:
+
+        OOI (2012). Data Product Specification for Velocity Profile and Echo
+            Intensity. Document Control Number 1341-00750.
+            https://alfresco.oceanobservatories.org/ (See: Company Home >> OOI
+            >> Controlled >> 1000 System Level >>
+            1341-00750_Data_Product_SPEC_VELPROF_OOI.pdf)
+
+        OOI (2013). Data Product Specification for Turbulent Velocity Profile
+            and Echo Intensity. Document Control Number 1341-00760.
+            https://alfresco.oceanobservatories.org/ (See: Company Home >> OOI
+            >> Controlled >> 1000 System Level >>
+            1341-00760_Data_Product_SPEC_VELPROF_OOI.pdf)
+    """
+    # force shapes of inputs to arrays
+    theta = np.atleast_1d(theta)
+    uv = np.atleast_2d(u)
+    v = np.atleast_2d(v)
+
+    theta_rad = np.radians(theta)
+    cosT = np.cos(theta_rad)
+    sinT = np.sin(theta_rad)
+
+    M = np.array([[cosT, sinT],
+                  [-sinT, cosT]])
+
+    # roll axes so that the lead index represents data packet #.
+    M = np.rollaxis(M, 2)
+
+    # the coordinate system is 2D, so the middle dimension is sized at 2.
+    uv = np.zeros((u.shape[0], 2, u.shape[1]))
+
+    # pack the coordinates to be rotated into the appropriate slices
+    uv[:, 0, :] = u
+    uv[:, 1, :] = v
+
+    # the Einstein summation is here configured to do the matrix
+    # multiplication uv_cor(i,k) = M(i,j) * uv(j,k) on each slice h.
+    uv_cor = np.einsum('hij,hjk->hik', M, uv)
+
+    # the magnetically corrected u values are:
+    u_cor = uv_cor[:, 0, :]
+
+    # the magnetically corrected v values are:
+    v_cor = uv_cor[:, 1, :]
+
+    # return corrected u and v values
+    return (u_cor, v_cor)
