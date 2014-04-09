8d91a151dc1eb0e94127f74d5e13f5d5aaf86dd8
Luke Campbell <luke.s.campbell@gmail.com>
2014-04-09 13:26:53 -0400

    Updates the gradient function
    - Adds detailed description of algorithm
    - New test to verify skipped values are treated correctly

--- extensions/gradient.c
+++ extensions/gradient.c
@@ -3,26 +3,72 @@
 #include <stdbool.h>
 #include "gradient.h"
 
-int gradient(signed char *out, const double *dat, const double *x, size_t len, double grad_min, double grad_max, double mindx, double startdat, double toldat, const double skipped_value)
-{
-    double ddatdx;
-    size_t i=0;
-    int j=0;
-    int skipped=0;
-    bool bad=false;
+/*
+ * gradient
+ * 
+ * The algorithm scans the data points in dat successively, one-by-one.
+ * Starting from ad ata point assumed to be good, if the change from this point
+ * to the next, divided by the respective advance in x, is within the range
+ * defined by ddatdx, the next point will be assumed good, else bad. Once a
+ * data point is identified as bad, successive data points will be considered
+ * bad until a data point falls within toldat of hte last known good data
+ * point.
+ *
+ * As a default starting point, the first data point is considered good. This
+ * default can be overridden by defining startdat, in which case the first data
+ * point that falls within toldat of startdat will be considered good (and
+ * potentially earlier ones as bad).
+ *
+ * x must be strictly increasing. In order to avoid small x steps, which can
+ * lead to exaggerated gradients because the x difference is used in the
+ * denominator, setting mindx to a value greater than zero is an option to
+ * remove all data points dat (and x) fo rwhich x is separated by mindx or
+ * less.
+ *
+ * Arguments:
+ * signed char *out           - An array of values that represent the evaluation of this QC. 
+ *                              Only bad values and skipped values are set, all others are 
+ *                              ignored. The implication here is that an array of ones,
+ *                              should be passed in.
+ * const double *dat          - An array of data values to apply QC against.
+ * const double *x            - The axis for the data values, must be in increasing order.
+ * size_t len                 - Length of out, dat and x. Each of these arrays
+ *                              MUST be the same size and match len.
+ * double grad_min            - Minimum change in dat over the change in x.
+ * double grad_max            - Maximum change in dat over the change in x.
+ * double mindx               - If dx is less than mindx, the value is skipped.
+ * double startdat            - Value to apply as a starting value (known good).
+ * double toldat              - The tolerance of d(dat)/dx to reach a good value.
+ * const double skipped_value - Caller specified value to apply when qc evaluation is 
+ *                              "skipped".
+ */
+
+int gradient(
+        signed char *out, 
+        const double *dat, 
+        const double *x, 
+        size_t len, 
+        double grad_min, 
+        double grad_max, 
+        double mindx, 
+        double startdat, 
+        double toldat, 
+        const double skipped_value) 
+{ 
+    double ddatdx; 
+    size_t i=0; 
+    int j=0; 
+    int skipped=0; 
+    bool bad=false; 
+
     if(startdat==0) { 
         /* If there was no startdat then set it to dat[0] */
-        startdat = dat[0];
-    }
-    else {
+        startdat = dat[0]; } else {
         /* startdat was specified */
         if(fabs(dat[0] - startdat) > toldat) {
-            /* If dat[0] is not within toldat of startdat then it's a bad value */
-            bad=true;
-            out[0] = 0;
-        }
-    }
-    for(i=1;i<len;i++) {
+            /* If dat[0] is not within toldat of startdat then it's a bad value
+             * */
+            bad=true; out[0] = -2; } } for(i=1;i<len;i++) {
 
         if (fabs((x[i]-x[i-(1+skipped)])) > mindx) {
             /* If the change in x is greater than mindx */
@@ -31,7 +77,7 @@ int gradient(signed char *out, const double *dat, const double *x, size_t len, d
                     bad = false;
                 }
                 else {
-                    out[i] = 0; /* still bad mark it and move on */
+                    out[i] = -3; /* still bad mark it and move on */
                 }
                 /* The test for this element is simple, if it's within the tolerance
                  * set it to startdat and continue on
@@ -45,15 +91,17 @@ int gradient(signed char *out, const double *dat, const double *x, size_t len, d
                 /* If the differential is outside of the min/max */
                 for(j=1;j<=skipped;j++) {
                     /* Set all the skipped to 0 as well */
-                    out[i-j] = 0;
+                    out[i-j] = -5;
                 }
                 skipped = 0; /* Reset the skipped */
-                out[i] = 0;  /* Set the output to false */
+                out[i] = -4;  /* Set the output to false */
                 bad=true;    /* Mark a bad */
             }
             else {
                 /* Continue on our way and update startdat */
                 startdat = dat[i];
+                /* Reset the skipped count, we're done skiping for the moment */
+                skipped = 0;
             }
         }
         else {
