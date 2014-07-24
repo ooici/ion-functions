58da58b0871d25f8e864d09cb6559a4d297441c0
Christopher Wingard <cwingard@coas.oregonstate.edu>
2014-07-24 14:18:46 -0700

    Adds test data, cleans up and finalizes OBS and HYD functions

--- ion_functions/data/test/test_hyd_functions.py
+++ ion_functions/data/test/test_hyd_functions.py
@@ -24,8 +24,9 @@ class TestHYDFunctionsUnit(BaseUnitTestCase):
         self.tsv = np.atleast_2d(np.array([-3.60855, -0.46911, 0.01669, 0.00665]))
 
         # setup the test values for HYDAPLF
-        self.raw = np.array([0, 1024, 2048, 3072, 4096])
-        self.hydaplf = np.array([])
+        self.raw = np.array([0, 512, 1024, 2048, 3072, 4096])
+        self.hydaplf = np.array([0.00000E+00, 1.63840E-03, 3.27680E-03,
+                                 6.55360E-03, 9.83040E-03, 1.31072E-02])
 
     def test_hyd_bb_acoustic_pwaves(self):
         """
@@ -42,7 +43,7 @@ class TestHYDFunctionsUnit(BaseUnitTestCase):
 
         Implemented by Christopher Wingard, May 2014
         """
-        # calculate the corrected compass direction
+        # calculate the broadband acoustic pressure wave
         out = hydfunc.hyd_bb_acoustic_pwaves(self.wav, self.gain)
 
         # How'd we do?
@@ -50,21 +51,15 @@ class TestHYDFunctionsUnit(BaseUnitTestCase):
 
     def test_hyd_lf_acoustic_pwaves(self):
         """
-        Test hyd_bb_acoustic_pwaves function.
+        Test hyd_lf_acoustic_pwaves function.
 
-        Values based on those described in DPS, with modification to account
-        for errors and inconsistencies in the DPS.
-
-        OOI (2013). Data Product Specification for Low Frequency Acoustic
-            Pressure Waves. Document Control Number 1341-00821.
-            https://alfresco.oceanobservatories.org/ (See: Company Home >>
-            OOI >> Controlled >> 1000 System Level >>
-            1341-00821_Data_Product_SPEC_HYDAPLF_OOI.pdf)
+        Values based on a test data set created using Excel by the test author
+        (DPS does not provide any test data).
 
         Implemented by Christopher Wingard, July 2014
         """
-        # calculate the corrected compass direction
+        # calculate the low frequency acoustic pressure wave
         out = hydfunc.hyd_lf_acoustic_pwaves(self.raw)
 
         # How'd we do?
-        np.testing.assert_allclose(out, self.raw, rtol=1e-6, atol=1e-6)
+        np.testing.assert_allclose(out, self.hydaplf, rtol=1e-6, atol=1e-6)
