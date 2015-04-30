3d113721d37daf597d40623297fa4f87106bcef0
Russell Desiderio <desi@coas.oregonstate.edu>
2015-04-30 09:20:18 -0700

    updated wavss unit tests to verify DPA time-vectorized compliance

--- ion_functions/data/test/test_wav_functions.py
+++ ion_functions/data/test/test_wav_functions.py
@@ -17,12 +17,15 @@ from ion_functions.utils import fill_value as vfill
 class TestWAVFunctionsUnit(BaseUnitTestCase):
 
     def setUp(self):
-        # for calculating dataproducts from # of values, initial value, and interval
-        self.nvalue = 5
-        self.value0 = 0.01
-        self.deltav = 0.001
-        self.ivalue = np.array([0.010, 0.011, 0.012, 0.013, 0.014], ndmin=2)
+        # repeat count for testing multiple record cases
         self.nrep = 10
+
+        # for calculating dataproducts from # of values, initial value, and interval
+        self.nvalue = np.array([5])
+        self.value0 = np.array([0.01])
+        self.deltav = np.array([0.001])
+        # resulting values are (2D; row vector)
+        self.ivalue = np.array([[0.010, 0.011, 0.012, 0.013, 0.014]])
 
         # for testing the correction of buoy displacement values for magnetic declination
         # (some values taken from the unit test module test_adcp_functions.py)
@@ -51,13 +54,16 @@ class TestWAVFunctionsUnit(BaseUnitTestCase):
             Company Home >> OOI >> Controlled >> 1000 System Level >>
             1341-00450_Data_Product_WAVE_STATISTICS_OOI.pdf)
 
-        Implemented by Russell Desiderio, 2014-04-08.
+        2014-04-08: Russell Desiderio.  Initial code.
+        2015-04-27: Russell Desiderio.  Verified that code is compliant with time-vectorized inputs.
         """
         # the single input record case
         desired = self.ivalue
         actual = wv.wav_triaxys_nondir_freq(self.nvalue, self.value0, self.deltav)
         # test
         np.testing.assert_allclose(actual, desired, rtol=1e-8, atol=0)
+        #print self.nvalue.shape, self.value0.shape, self.deltav.shape
+        #print actual.shape, desired.shape
 
         # the multi-record case -- inputs
         nvalues = np.repeat(self.nvalue, self.nrep)
@@ -68,6 +74,8 @@ class TestWAVFunctionsUnit(BaseUnitTestCase):
         actual = wv.wav_triaxys_nondir_freq(nvalues, value0s, deltavs)
         # test
         np.testing.assert_allclose(actual, desired, rtol=1e-8, atol=0)
+        #print nvalues.shape, value0s.shape, deltavs.shape
+        #print actual.shape, desired.shape
 
     def test_wav_triaxys_dir_freq(self):
         """
@@ -80,13 +88,16 @@ class TestWAVFunctionsUnit(BaseUnitTestCase):
             Company Home >> OOI >> Controlled >> 1000 System Level >>
             1341-00450_Data_Product_WAVE_STATISTICS_OOI.pdf)
 
-        Implemented by Russell Desiderio, 2014-04-08.
+        2014-04-08: Russell Desiderio.  Initial code.
+        2015-04-27: Russell Desiderio.  Verified that code is compliant with time-vectorized inputs.
         """
         # the single input record case
         desired = self.ivalue
         actual = wv.wav_triaxys_dir_freq(self.nvalue, self.nvalue, self.value0, self.deltav)
         # test
         np.testing.assert_allclose(actual, desired, rtol=1e-8, atol=0)
+        #print self.nvalue.shape, self.value0.shape, self.deltav.shape
+        #print actual.shape, desired.shape
 
         # the multi-record case -- all nvalues_dir are equal -- inputs
         nvalues = np.repeat(self.nvalue, self.nrep)
@@ -97,6 +108,8 @@ class TestWAVFunctionsUnit(BaseUnitTestCase):
         actual = wv.wav_triaxys_dir_freq(nvalues, nvalues, value0s, deltavs)
         # test
         np.testing.assert_allclose(actual, desired, rtol=1e-8, atol=0)
+        #print nvalues.shape, value0s.shape, deltavs.shape
+        #print actual.shape, desired.shape
 
         # the multi-record case -- all nvalues_dir are not the same -- inputs
         nvalues_nondir = np.repeat(self.nvalue, self.nrep)
@@ -115,9 +128,12 @@ class TestWAVFunctionsUnit(BaseUnitTestCase):
                             [0.010, 0.011, 0.012, 0.013, 0.014],
                             [0.010, 0.011, 0.012, vfill, vfill]])
 
+        #print nvalues_nondir.shape, nvalues_dir.shape, value0s.shape, deltavs.shape
         actual = wv.wav_triaxys_dir_freq(nvalues_nondir, nvalues_dir, value0s, deltavs)
         # test
         np.testing.assert_allclose(actual, desired, rtol=1e-8, atol=0)
+        #print nvalues_nondir.shape, nvalues_dir.shape, value0s.shape, deltavs.shape
+        #print actual.shape, desired.shape
 
     def test_wav_triaxys_buoymotion_time(self):
         """
@@ -131,22 +147,39 @@ class TestWAVFunctionsUnit(BaseUnitTestCase):
             Company Home >> OOI >> Controlled >> 1000 System Level >>
             1341-00450_Data_Product_WAVE_STATISTICS_OOI.pdf)
 
-        Implemented by Russell Desiderio, 2014-04-08.
+        2014-04-08: Russell Desiderio.  Initial code.
+        2015-04-30: Russell Desiderio.  Revised multi-record case to use time-vectorized
+                                        ntp_timestamps; the DPA algorithm itself
+                                        (wav_triaxys_buoymotion_time) was correctly coded
+                                        and did not need to be modified.
         """
         # the single input record case
-        ntp_timestamp = 3176736750.736
+        ntp_timestamp = np.array([3176736750.736])
         desired = self.ivalue + ntp_timestamp
         actual = wv.wav_triaxys_buoymotion_time(ntp_timestamp, self.nvalue, self.value0, self.deltav)
         # test to msec
         np.testing.assert_allclose(actual, desired, rtol=0, atol=0.001)
+        #print ntp_timestamp.shape, self.nvalue.shape, self.value0.shape, self.deltav.shape
+        #print actual.shape, desired.shape
 
         # the multi-record case -- inputs
-        nvalues = np.repeat(self.nvalue, self.nrep)
-        value0s = np.repeat(self.value0, self.nrep)
-        deltavs = np.repeat(self.deltav, self.nrep)
+        t_rep = 3
+        nvalues = np.repeat(self.nvalue, t_rep)
+        value0s = np.repeat(self.value0, t_rep)
+        deltavs = np.repeat(self.deltav, t_rep)
+        # time vectorize ntp_timestamp, too; pick a 1 hr sampling interval
+        ntp_timestamp = ntp_timestamp + np.array([0.0, 3600.0, 7200.0])
         # the multi-record case -- outputs
-        desired = np.tile(self.ivalue + ntp_timestamp, (self.nrep, 1))
+        # reshape timestamp array into a column vector then tile to number of ivalue columns
+        ntp_2D = np.tile(ntp_timestamp.reshape(-1, 1), (1, self.ivalue.shape[-1]))
+        desired = ntp_2D + np.tile(self.ivalue, (t_rep, 1))
         actual = wv.wav_triaxys_buoymotion_time(ntp_timestamp, nvalues, value0s, deltavs)
+
+        #print ntp_timestamp.shape, nvalues.shape, value0s.shape, deltavs.shape
+        #ntp_floor = np.floor(ntp_timestamp[0])
+        #print actual.shape, actual - ntp_floor
+        #print desired.shape, desired - ntp_floor
+
         # test to msec
         np.testing.assert_allclose(actual, desired, rtol=0, atol=0.001)
 
@@ -164,17 +197,20 @@ class TestWAVFunctionsUnit(BaseUnitTestCase):
             Company Home >> OOI >> Controlled >> 1000 System Level >>
             1341-00450_Data_Product_WAVE_STATISTICS_OOI.pdf)
 
-        Implemented by Russell Desiderio, 2014-04-08.
+        2014-04-08: Russell Desiderio.  Initial code.
+        2015-04-27: Russell Desiderio.  Verified that code is compliant with time-vectorized inputs.
         """
         # the single input record case
-        dir_raw = 50.0
-        lat = 45.0
-        lon = -128.0
-        ntp_ts = 3575053740.0
-        desired = 50.0 + 16.461005
+        dir_raw = np.array([50.0])
+        lat = np.array([45.0])
+        lon = np.array([-128.0])
+        ntp_ts = np.array([3575053740.0])
+        desired = np.array([50.0 + 16.461005])
         actual = wv.wav_triaxys_correct_mean_wave_direction(dir_raw, lat, lon, ntp_ts)
         # test to first decimal place, in case model changes
         np.testing.assert_allclose(actual, desired, rtol=0, atol=0.1)
+        #print dir_raw.shape, lat.shape, lon.shape, ntp_ts.shape
+        #print actual.shape, desired.shape
 
         # the multi-record case -- inputs
         # test "going around the corner" in both directions.
@@ -187,6 +223,8 @@ class TestWAVFunctionsUnit(BaseUnitTestCase):
         actual = wv.wav_triaxys_correct_mean_wave_direction(dir_raw, lat, lon, ntp_ts)
         # test
         np.testing.assert_allclose(actual, desired, rtol=0, atol=0.1)
+        #print dir_raw.shape, lat.shape, lon.shape, ntp_ts.shape
+        #print actual.shape, desired.shape
 
     def test_wav_triaxys_correct_directional_wave_direction(self):
         """
@@ -202,29 +240,34 @@ class TestWAVFunctionsUnit(BaseUnitTestCase):
             Company Home >> OOI >> Controlled >> 1000 System Level >>
             1341-00450_Data_Product_WAVE_STATISTICS_OOI.pdf)
 
-        Implemented by Russell Desiderio, 2014-04-10.
+        2014-04-10: Russell Desiderio.  Initial code.
+        2015-04-27: Russell Desiderio.  Verified that code is compliant with time-vectorized inputs.
         """
         # the single input record case - no fill values (nfreq_dir = nfreq_nondir)
-        dir_raw = np.array([50.0, 1.0, 359.0, 180.0, 245.0])
-        lat = 45.0
-        lon = -128.0
-        ntp_ts = 3575053740.0
+        dir_raw = np.array([[50.0, 1.0, 359.0, 180.0, 245.0]])
+        lat = np.array([45.0])
+        lon = np.array([-128.0])
+        ntp_ts = np.array([3575053740.0])
         # outputs
-        desired = np.array([66.461, 17.461, 15.461, 196.461, 261.461], ndmin=2)
+        desired = np.array([[66.461, 17.461, 15.461, 196.461, 261.461]])
         actual = wv.wav_triaxys_correct_directional_wave_direction(dir_raw, lat, lon, ntp_ts)
         # test to first decimal place, in case model changes
         np.testing.assert_allclose(actual, desired, rtol=0, atol=0.1)
+        #print dir_raw.shape, lat.shape, lon.shape, ntp_ts.shape
+        #print actual.shape, desired.shape
 
         # the single input record case - with fill values (nfreq_dir < nfreq_nondir)
-        dir_raw = np.array([50.0, 1.0, 359.0, vfill, vfill])
-        lat = 45.0
-        lon = -128.0
-        ntp_ts = 3575053740.0
+        dir_raw = np.array([[50.0, 1.0, 359.0, vfill, vfill]])
+        lat = np.array([45.0])
+        lon = np.array([-128.0])
+        ntp_ts = np.array([3575053740.0])
         # outputs
-        desired = np.array([66.461, 17.461, 15.461, vfill, vfill], ndmin=2)
+        desired = np.array([[66.461, 17.461, 15.461, vfill, vfill]])
         actual = wv.wav_triaxys_correct_directional_wave_direction(dir_raw, lat, lon, ntp_ts)
         # test to first decimal place, in case model changes
         np.testing.assert_allclose(actual, desired, rtol=0, atol=0.1)
+        #print dir_raw.shape, lat.shape, lon.shape, ntp_ts.shape
+        #print actual.shape, desired.shape
 
         # the multi-record case -- inputs
         # test "going around the corner" in both directions.
@@ -241,6 +284,8 @@ class TestWAVFunctionsUnit(BaseUnitTestCase):
         actual = wv.wav_triaxys_correct_directional_wave_direction(dir_raw, lat, lon, ntp_ts)
         # test
         np.testing.assert_allclose(actual, desired, rtol=0, atol=0.1)
+        #print dir_raw.shape, lat.shape, lon.shape, ntp_ts.shape
+        #print actual.shape, desired.shape
 
     def test_wav_triaxys_magcor_buoymotion_x(self):
         """
@@ -253,25 +298,30 @@ class TestWAVFunctionsUnit(BaseUnitTestCase):
             Company Home >> OOI >> Controlled >> 1000 System Level >>
             1341-00450_Data_Product_WAVE_STATISTICS_OOI.pdf)
 
-        Implemented by Russell Desiderio, 2014-04-10.
+        2014-04-10: Russell Desiderio.  Initial code.
+        2015-04-27: Russell Desiderio.  Verified that code is compliant with time-vectorized inputs.
         """
         # the single input record case
-        lat = self.lat[0]
-        lon = self.lon[0]
-        ntp = self.ntp[0]
-        xx = self.xx[0, :]
-        yy = self.yy[0, :]
+        lat = self.lat[[0]]
+        lon = self.lon[[0]]
+        ntp = self.ntp[[0]]
+        xx = self.xx[[0], :]
+        yy = self.yy[[0], :]
         # outputs
-        desired = np.array(self.xx_cor[0, :], ndmin=2)
+        desired = self.xx_cor[[0], :]
         actual = wv.wav_triaxys_magcor_buoymotion_x(xx, yy, lat, lon, ntp)
         # test
         np.testing.assert_allclose(actual, desired, rtol=0, atol=0.0001)
+        #print xx.shape, yy.shape, lat.shape, lon.shape, ntp.shape
+        #print actual.shape, desired.shape
 
         # multiple records
         desired = np.array(self.xx_cor)
         actual = wv.wav_triaxys_magcor_buoymotion_x(self.xx, self.yy, self.lat, self.lon, self.ntp)
         # test
         np.testing.assert_allclose(actual, desired, rtol=0, atol=0.0001)
+        #print self.xx.shape, self.yy.shape, self.lat.shape, self.lon.shape, self.ntp.shape
+        #print actual.shape, desired.shape
 
     def test_wav_triaxys_magcor_buoymotion_y(self):
         """
@@ -284,22 +334,27 @@ class TestWAVFunctionsUnit(BaseUnitTestCase):
             Company Home >> OOI >> Controlled >> 1000 System Level >>
             1341-00450_Data_Product_WAVE_STATISTICS_OOI.pdf)
 
-        Implemented by Russell Desiderio, 2014-04-10.
+        2014-04-10: Russell Desiderio.  Initial code.
+        2015-04-27: Russell Desiderio.  Verified that code is compliant with time-vectorized inputs.
         """
         # the single input record case
-        lat = self.lat[0]
-        lon = self.lon[0]
-        ntp = self.ntp[0]
-        xx = self.xx[0, :]
-        yy = self.yy[0, :]
+        lat = self.lat[[0]]
+        lon = self.lon[[0]]
+        ntp = self.ntp[[0]]
+        xx = self.xx[[0], :]
+        yy = self.yy[[0], :]
         # outputs
-        desired = np.array(self.yy_cor[0, :], ndmin=2)
+        desired = self.yy_cor[[0], :]
         actual = wv.wav_triaxys_magcor_buoymotion_y(xx, yy, lat, lon, ntp)
         # test
         np.testing.assert_allclose(actual, desired, rtol=0, atol=0.0001)
+        #print xx.shape, yy.shape, lat.shape, lon.shape, ntp.shape
+        #print actual.shape, desired.shape
 
         # multiple records
         desired = np.array(self.yy_cor)
         actual = wv.wav_triaxys_magcor_buoymotion_y(self.xx, self.yy, self.lat, self.lon, self.ntp)
         # test
         np.testing.assert_allclose(actual, desired, rtol=0, atol=0.0001)
+        #print self.xx.shape, self.yy.shape, self.lat.shape, self.lon.shape, self.ntp.shape
+        #print actual.shape, desired.shape
